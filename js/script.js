54a60bdbd807e81128765d26d372282a8705f423
MarcoB <mrc.bsllt@gmail.com>
2020-11-19 21:56:11 +0100

    completato il bonus

--- js/script.js
+++ js/script.js
@@ -21,33 +21,33 @@ function isThereAlreadyThisNumber (array, number) {
 
 // ----------------------------funzioni
 
-var difficult = document.getElementById("bombs");
+var difficult = document.getElementById("difficult");
 var button = document.getElementById("button");
 var bombs = document.getElementById("bombs");
 var userNumbers = document.getElementById("user_numbers");
+var arrayBombs = [];
+var arrayUser = [];
 
 button.addEventListener("click",
   function() {
 
-    var arrayBombs = [];
-    var arrayUser = [];
-
+    difficult = difficult.value;
     // IMPOSTO IL BONUS
-    // switch (difficult) {
-    //   case "Easy";
-    //     pcNumber = randomNUmber(1, 100);
-    //     break;
-    //   case "Normal";
-    //     pcNumber = randomNUmber(1, 80);
-    //     break;
-    //   case "Hard";
-    //     pcNumber = randomNUmber(1, 50);
-    //     break;
-    // }
-
-    // genero 16 numeri casuali da 1 a 100 tutti diversi
+    var maxNumber = 0;
+    switch (difficult) {
+      case "Easy":
+        maxNumber = 100;
+        break;
+      case "Normal":
+        maxNumber = 80;
+        break;
+      case "Hard":
+        maxNumber = 50;
+        break;
+    }
+    // genero 16 numeri casuali da 1 a maxNumber tutti diversi
     for (var i = 0; i < 16; i++) {
-      var pcNumber = randomNUmber(1, 100);
+      var pcNumber = randomNUmber(1, maxNumber);
       var checkPcNumber = isThereAlreadyThisNumber(arrayBombs, pcNumber);
       if (checkPcNumber == true) {
         // se un numero viene duplicato non lo inserisco e decremento la i
@@ -61,11 +61,11 @@ button.addEventListener("click",
 
     // devo chiedere all'utente di inserire per 84 volte un numero tra 1 e 100
     var score = 0;
-    for (var i = 0; i < 84; i++) {
+    for (var i = 0; i < (maxNumber - 16); i++) {
       // chiedo all'utente di inserire un numero
-      var userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
+      var userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a " + maxNumber));
       // se il numero inserito non è un numero, non è compreso da 1 a 10 e non è già stato inserito, errore!
-      while ((isNaN(userNumber)) || ((userNumber < 1) || (userNumber > 100) || (isThereAlreadyThisNumber(arrayUser, userNumber)))) {
+      while ((isNaN(userNumber)) || ((userNumber < 1) || (userNumber > maxNumber) || (isThereAlreadyThisNumber(arrayUser, userNumber)))) {
         alert("Errore!");
         userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
       }
@@ -77,7 +77,7 @@ button.addEventListener("click",
         userNumbers.innerHTML += "<li class='red'>" + userNumber + "</li>";
         bombs.className = "red";
         bombs.innerHTML = userNumber;
-        i = 84;
+        i = maxNumber - 16;
       } else {
         // se il numero valido inserito non è neanche una bomba lo inserisco nell' Array dei numeri inseriti dall'utente
         arrayUser.push(userNumber);
