b9f29f181cc9cc585af5c1acc798a57675b9a948
MarcoB <mrc.bsllt@gmail.com>
2020-11-19 21:35:52 +0100

    aggiunto attivazione con bottone e sistemato un bug nel controllo numero utente inserito

--- js/script.js
+++ js/script.js
@@ -20,67 +20,74 @@ function isThereAlreadyThisNumber (array, number) {
 }
 
 // ----------------------------funzioni
-var difficult = document.getElementById("bombs").value;
+
+var difficult = document.getElementById("bombs");
+var button = document.getElementById("button");
 var bombs = document.getElementById("bombs");
 var userNumbers = document.getElementById("user_numbers");
 
-var arrayBombs = [];
-var arrayUser = [];
+button.addEventListener("click",
+  function() {
 
-// IMPOSTO IL BONUS
-// switch (difficult) {
-//   case "Easy";
-//     pcNumber = randomNUmber(1, 100);
-//     break;
-//   case "Normal";
-//     pcNumber = randomNUmber(1, 80);
-//     break;
-//   case "Hard";
-//     pcNumber = randomNUmber(1, 50);
-//     break;
-// }
+    var arrayBombs = [];
+    var arrayUser = [];
 
-// genero 16 numeri casuali da 1 a 100 tutti diversi
-for (var i = 0; i < 16; i++) {
-  var pcNumber = randomNUmber(1, 100);
-  var checkPcNumber = isThereAlreadyThisNumber(arrayBombs, pcNumber);
-  if (checkPcNumber == true) {
-    // se un numero viene duplicato non lo inserisco e decremento la i
-    i--;
-  } else {
-    // inserisco il numero nell' Array delle bombe
-    arrayBombs.push(pcNumber);
-  }
-}
-console.log(arrayBombs);
+    // IMPOSTO IL BONUS
+    // switch (difficult) {
+    //   case "Easy";
+    //     pcNumber = randomNUmber(1, 100);
+    //     break;
+    //   case "Normal";
+    //     pcNumber = randomNUmber(1, 80);
+    //     break;
+    //   case "Hard";
+    //     pcNumber = randomNUmber(1, 50);
+    //     break;
+    // }
 
-// devo chiedere all'utente di inserire per 84 volte un numero tra 1 e 100
-var score = 0;
-for (var i = 0; i < 84; i++) {
-  // chiedo all'utente di inserire un numero
-  var userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
-  // se il numero inserito non è un numero, non è compreso da 1 a 10 e non è già stato inserito, errore!
-  if ((isNaN(userNumber)) || ((userNumber < 1) || (userNumber > 100) || (isThereAlreadyThisNumber(arrayUser, userNumber)))) {
-    alert("Errore!");
-    userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
+    // genero 16 numeri casuali da 1 a 100 tutti diversi
+    for (var i = 0; i < 16; i++) {
+      var pcNumber = randomNUmber(1, 100);
+      var checkPcNumber = isThereAlreadyThisNumber(arrayBombs, pcNumber);
+      if (checkPcNumber == true) {
+        // se un numero viene duplicato non lo inserisco e decremento la i
+        i--;
+      } else {
+        // inserisco il numero nell' Array delle bombe
+        arrayBombs.push(pcNumber);
+      }
+    }
+    console.log(arrayBombs);
+
+    // devo chiedere all'utente di inserire per 84 volte un numero tra 1 e 100
+    var score = 0;
+    for (var i = 0; i < 84; i++) {
+      // chiedo all'utente di inserire un numero
+      var userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
+      // se il numero inserito non è un numero, non è compreso da 1 a 10 e non è già stato inserito, errore!
+      while ((isNaN(userNumber)) || ((userNumber < 1) || (userNumber > 100) || (isThereAlreadyThisNumber(arrayUser, userNumber)))) {
+        alert("Errore!");
+        userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
+      }
+      // controllo che il numero valido inserito non sia presente tra le bombe
+      var isAbomb = isThereAlreadyThisNumber(arrayBombs, userNumber);
+      if (isAbomb) {
+        // se è presente tra le bombe, HAI PERSO! e finisce il programma
+        alert("Hai perso! il numero: " + userNumber + " è una BOMBA!");
+        userNumbers.innerHTML += "<li class='red'>" + userNumber + "</li>";
+        bombs.className = "red";
+        bombs.innerHTML = userNumber;
+        i = 84;
+      } else {
+        // se il numero valido inserito non è neanche una bomba lo inserisco nell' Array dei numeri inseriti dall'utente
+        arrayUser.push(userNumber);
+        userNumbers.innerHTML += "<li>" + userNumber + "</li>";
+        score++;
+      }
+    }
+    // se l'utente è stato così fortunato da non beccare mai un numero bomba, HAI VINTO!
+    if (isAbomb == false) {
+      alert("COMPLIMENTI HAI VINTO!");
+    }
   }
-  // controllo che il numero valido inserito non sia presente tra le bombe
-  var isAbomb = isThereAlreadyThisNumber(arrayBombs, userNumber);
-  if (isAbomb) {
-    // se è presente tra le bombe, HAI PERSO! e finisce il programma
-    alert("Hai perso! il numero: " + userNumber + " è una BOMBA!");
-    userNumbers.innerHTML += "<li class='red'>" + userNumber + "</li>";
-    bombs.className = "red";
-    bombs.innerHTML = userNumber;
-    i = 84;
-  } else {
-    // se il numero valido inserito non è neanche una bomba lo inserisco nell' Array dei numeri inseriti dall'utente
-    arrayUser.push(userNumber);
-    userNumbers.innerHTML += "<li>" + userNumber + "</li>";
-    score++;
-  }
-}
-// se l'utente è stato così fortunato da non beccare mai un numero bomba, HAI VINTO!
-if (isAbomb == false) {
-  alert("COMPLIMENTI HAI VINTO!");
-}
+);
