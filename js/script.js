91657360103562c7f5feb3661c50171657c516dd
MarcoB <mrc.bsllt@gmail.com>
2020-11-19 18:52:38 +0100

    impostato lo switch per il bonus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati.
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
 
 // ----------------------------funzioni
 function randomNUmber (min, max) {
@@ -22,12 +20,25 @@ function isThereAlreadyThisNumber (array, number) {
 }
 
 // ----------------------------funzioni
-
+var difficult = document.getElementById("bombs").value;
 var bombs = document.getElementById("bombs");
 var userNumbers = document.getElementById("user_numbers");
 
 var arrayBombs = [];
 var arrayUser = [];
+
+// IMPOSTO IL BONUS
+// switch (difficult) {
+//   case "Easy";
+//     pcNumber = randomNUmber(1, 100);
+//     break;
+//   case "Normal";
+//     pcNumber = randomNUmber(1, 80);
+//     break;
+//   case "Hard";
+//     pcNumber = randomNUmber(1, 50);
+//     break;
+// }
 
 // genero 16 numeri casuali da 1 a 100 tutti diversi
 for (var i = 0; i < 16; i++) {
