5bc8acc0825e3f35ebcde3f8052819cc7e5e16f4
MarcoB <mrc.bsllt@gmail.com>
2020-11-19 14:00:32 +0100

    scritto i commenti con i vari passaggi da fare e creato funzione per numero casuale

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("ciao");
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// ----------------------------funzioni
+function randomNUmber (min, max) {
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
+console.log(randomNUmber(1, 100));
+// ----------------------------funzioni
+
+
+
+// creo un Array che dovà contenere dei numeri, ovvero le bombe
+var arraybombs = [];
+
+// creo una funzione che mi permetta di generare un numero casuale tra 1 e 100
+function randomNUmber (min, max) {
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
+console.log(randomNUmber(1, 100));
+// devo generare 16 numeri random dal computer, ovvero le bombe
+
+// inserisco i 16 numeri random generati all'interno dell'Array
+
+// devo chiedere all'utente di inserire per 16 volte un numero tra 1 e 100
+
+// devo assicurarmi che il numero inserita sia intero e non può inserire un numero doppio
+
+// se inserisce un numero già presente nell'Array ha perso e la partita termina
+
+// se si finisco le possibilità di scelta la partita termina e vince
+
+// calcolare il punteggio dell'utente, ogni numero azzeccato equivale ad un punto
