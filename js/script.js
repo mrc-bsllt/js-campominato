1d26abdaf0f1fffbda11957573f889cb057cce55
MarcoB <mrc.bsllt@gmail.com>
2020-11-19 18:10:11 +0100

    aggiunto i commenti nel codice js

--- js/script.js
+++ js/script.js
@@ -26,48 +26,46 @@ function isThereAlreadyThisNumber (array, number) {
 var bombs = document.getElementById("bombs");
 var userNumbers = document.getElementById("user_numbers");
 
-
-// creo un Array che dovà contenere dei numeri, ovvero le bombe
 var arrayBombs = [];
 var arrayUser = [];
 
-// devo generare 16 numeri random DIVERSI dal computer, ovvero le bombe
-// inserisco i 16 numeri random generati all'interno dell'Array
-
+// genero 16 numeri casuali da 1 a 16 tutti diversi
 for (var i = 0; i < 16; i++) {
-  var pcNumber = randomNUmber(1, 100);
+  var pcNumber = randomNUmber(1, 16);
   var checkPcNumber = isThereAlreadyThisNumber(arrayBombs, pcNumber);
   if (checkPcNumber == true) {
+    // se un numero viene duplicato non lo inserisco e decremento la i
     i--;
   } else {
+    // inserisco il numero nell' Array delle bombe
     arrayBombs.push(pcNumber);
   }
 }
 console.log(arrayBombs);
+
 // devo chiedere all'utente di inserire per 84 volte un numero tra 1 e 100
 var score = 0;
 for (var i = 0; i < 84; i++) {
+  // chiedo all'utente di inserire un numero
   var userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
+  // se il numero inserito non è un numero, non è compreso da 1 a 10 e non è già stato inserito, errore!
   if ((isNaN(userNumber)) || ((userNumber < 1) || (userNumber > 100) || (isThereAlreadyThisNumber(arrayUser, userNumber)))) {
     alert("Errore!");
     userNumber = parseInt(prompt("inserisci il " + (i+1) + "°" + " numero da 1 a 100"));
   }
+  // controllo che il numero valido inserito non sia presente tra le bombe
   var isAbomb = isThereAlreadyThisNumber(arrayBombs, userNumber);
   if (isAbomb) {
+    // se è presente tra le bombe, HAI PERSO! e finisce il programma
     alert("Hai perso! il numero: " + userNumber + " è una BOMBA!");
     i = 84;
   } else {
+    // se il numero valido inserito non è neanche una bomba lo inserisco nell' Array dei numeri inseriti dall'utente
     arrayUser.push(userNumber);
     score++;
   }
 }
+// se l'utente è stato così fortunato da non beccare mai un numero bomba, HAI VINTO!
 if (isAbomb == false) {
   alert("COMPLIMENTI HAI VINTO!");
 }
-// devo assicurarmi che il numero inserita sia intero e non può inserire un numero doppio
-
-// se inserisce un numero già presente nell'Array ha perso e la partita termina
-
-// se si finisco le possibilità di scelta la partita termina e vince
-
-// calcolare il punteggio dell'utente, ogni numero azzeccato equivale ad un punto
